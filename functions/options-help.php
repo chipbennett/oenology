5d623b497eab81bdd49a0d3971b9195935078512
Chip Bennett <chip@chipbennett.net>
2011-02-10 12:09:02 -0600

    Fun with Heredoc in contextual help

--- functions/options-help.php
+++ functions/options-help.php
@@ -21,12 +21,6 @@ switch ( $tab ) {
 function oenology_get_contextual_help_options_general() {
 
 	$tabtext = '';
-	/*$tabtext .= "<h2>" . __( 'General Options', 'oenology' ) . "</h2>";
-    $tabtext .= "<h3>" . __( 'Header Nav Menu Position', 'oenology' ) . "</h3>";
-    $tabtext .= "<p>" . __('The default location of the header navigation menu is above the site title/description. Use this setting to display the header navigation menu below the site title/description.', 'oenology') . "</p>";
-    $tabtext .= "<h3>" . __( 'Footer Credit', 'oenology' ) . "</h3>";
-    $tabtext .= "<p>" . __('This setting controls the display of a footer credit link. By default, no footer credit link is displayed. You are under no obligation to display a credit link in the footer or anywhere else.', 'oenology') . "</p>";
-	*/
 	$tabtext .= <<<EOT
 	<h2>General Options</h2>
 	<h3>Header Nav Menu Position</h3>
@@ -42,14 +36,18 @@ EOT;
 function oenology_get_contextual_help_options_varietals() {
 
 	$tabtext = '';
-	$tabtext .= "<h2>" . __( 'Varietals', 'oenology' ) . "</h2>";
-    $tabtext .= "<p>" . __('"Varietals" are the "skins" or styles applied to Oenology.', 'oenology') . "</p>";
+	$tabtext .= <<<EOT
+	<h2>Varietals</h2>
+	<p><em>Varietals</em> are the <em>skins</em>, or styles, applied to Oenology.</p>
+EOT;
     $oenology_varietals = oenology_get_valid_varietals();
     foreach ( $oenology_varietals as $varietal ) {
-	    $tabtext .= "<dl>";
-	    $tabtext .= "<dt><strong>" . $varietal['name'] . "</strong></dt>";
-	    $tabtext .= "<dd>" . $varietal['description'] . "</dd>";
-	    $tabtext .= "</dl>";
+	    $tabtext .= <<<EOT
+		<dl>
+		<dt><strong>{$varietal['name']}</strong></dt>
+		<dd>{$varietal['description']}</dd>
+		</dl>
+EOT;
 	}
 	return $tabtext;
 }
