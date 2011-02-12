0c17bf3f4b2f051fa64fae80743f4111ec598a7a
Chip Bennett <chip@chipbennett.net>
2011-02-11 21:12:41 -0600

    General Options tweaks, Post box shadows for Seyval Blanc, Seyval

--- functions/options-register-general.php
+++ functions/options-register-general.php
@@ -46,43 +46,31 @@ function oenology_settings_general_footer_section_text() { ?>
 // Navigation Menu Position Setting
 function oenology_setting_header_nav_menu_position() {
 	$oenology_options = get_option( 'theme_oenology_options' ); ?>
-	<p>
-		<label for="oenology_header_nav_menu_position">
-			Display header navigation menu above or below the site title/description?<br />
-			<select name="theme_oenology_options[header_nav_menu_position]">
-				<option <?php selected( 'above' == $oenology_options['header_nav_menu_position'] ); ?> value="above">Above</option>
-				<option <?php selected( 'below' == $oenology_options['header_nav_menu_position'] ); ?> value="below">Below</option>
-			</select>
-		</label>
-	</p>
+	<select name="theme_oenology_options[header_nav_menu_position]">
+		<option <?php selected( 'above' == $oenology_options['header_nav_menu_position'] ); ?> value="above">Above</option>
+		<option <?php selected( 'below' == $oenology_options['header_nav_menu_position'] ); ?> value="below">Below</option>
+	</select>
+	<span class="description">Display header navigation menu above or below the site title/description?</span>
 <?php }
 
 // Navigation Menu Position Depth
 function oenology_setting_header_nav_menu_depth() {
-	$oenology_options = get_option( 'theme_oenology_options' ); ?>
-	<p>
-		<label for="oenology_header_nav_menu_depth">
-			How many levels of Page hierarchy should the Header Navigation Menu display?<br />
-			<select name="theme_oenology_options[header_nav_menu_depth]">
-				<option <?php selected( 1 == $oenology_options['header_nav_menu_depth'] ); ?> value="1">One</option>
-				<option <?php selected( 2 == $oenology_options['header_nav_menu_depth'] ); ?> value="2">Two</option>
-				<option <?php selected( 3 == $oenology_options['header_nav_menu_depth'] ); ?> value="3">Three</option>
-			</select>
-		</label>
-	</p>
+	$oenology_options = get_option( 'theme_oenology_options' ); ?>	
+	<select name="theme_oenology_options[header_nav_menu_depth]">
+		<option <?php selected( 1 == $oenology_options['header_nav_menu_depth'] ); ?> value="1">One</option>
+		<option <?php selected( 2 == $oenology_options['header_nav_menu_depth'] ); ?> value="2">Two</option>
+		<option <?php selected( 3 == $oenology_options['header_nav_menu_depth'] ); ?> value="3">Three</option>
+	</select>
+	<span class="description">How many levels of Page hierarchy should the Header Navigation Menu display?</span>
 <?php }
 
 // Display Footer Credit Setting
 function oenology_setting_display_footer_credit() {
 	$oenology_options = get_option( 'theme_oenology_options' ); ?>
-	<p>
-		<label for="oenology_display_footer_credit">
-			Display a credit link in the footer? This option is disabled by default, and you are under no obligation whatsoever to enable it.<br />
-			<select name="theme_oenology_options[display_footer_credit]">
-				<option <?php selected( false == $oenology_options['display_footer_credit'] ); ?> value="false">Do Not Display</option>
-				<option <?php selected( true == $oenology_options['display_footer_credit'] ); ?> value="true">Display</option>
-			</select>
-		</label>
-	</p>
+	<select name="theme_oenology_options[display_footer_credit]">
+		<option <?php selected( false == $oenology_options['display_footer_credit'] ); ?> value="false">Do Not Display</option>
+		<option <?php selected( true == $oenology_options['display_footer_credit'] ); ?> value="true">Display</option>
+	</select>
+	<span class="description">Display a credit link in the footer? This option is disabled by default, and you are under no obligation whatsoever to enable it.</span>
 <?php }
 ?>
