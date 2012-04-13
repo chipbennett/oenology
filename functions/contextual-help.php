a70357e1699e6d91aff2d74ac496eedef55d23ea
Chip Bennett <chip@chipbennett.net>
2012-04-13 14:43:33 -0500

    Re: Issue #40 update docs and replace heredoc with file_get_contents() for contextual help tabs

--- functions/contextual-help.php
+++ functions/contextual-help.php
@@ -18,6 +18,16 @@
  * 
  * Contextual help, WordPress 3.3-compatible
  * 
+ * This callback is hooked into the load-$oenology_settings_page hook,
+ * via the oenology_add_theme_page() callback, which is hooked into the
+ * admin_menu hook, and which is defined in /functions/options.php.
+ * 
+ * @link 		http://codex.wordpress.org/Function_Reference/add_help_tab				add_help_tab()
+ * @link 		http://codex.wordpress.org/Function_Reference/get_current_screen		get_current_screen()
+ * @link 		http://codex.wordpress.org/Function_Reference/get_template_directory	get_template_directory()
+ * 
+ * @link 		http://php.net/manual/en/function.file-get-contents.php					file_get_contents()
+ * 
  * @since	Oenology 2.5
  */
 function oenology_settings_page_contextual_help() {
@@ -32,55 +42,55 @@ function oenology_settings_page_contextual_help() {
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-settings-varietal',
 		'title'   => __( 'Settings - Varietals', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-settings-varietal.php' ),
+		'content' => file_get_contents( get_template_directory() . '/help/settings-varietal.htm' ),
 	) );
 	// Add Settings - Layout help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-settings-layout',
 		'title'   => __( 'Settings - Layout', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-settings-layout.php' ),
+		'content' => file_get_contents( get_template_directory() . '/help/settings-layout.htm' ),
 	) );
 	// Add Settings - General help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-settings-general',
 		'title'   => __( 'Settings - General', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-settings-general.php' ),
+		'content' => file_get_contents( get_template_directory() . '/help/settings-general.htm' ),
 	) );
 	// Add Theme Features help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-features',
 		'title'   => __( 'Theme Features', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-features.php' ),
+		'content' => file_get_contents( get_template_directory() . '/help/features.htm' ),
 	) );
 	// Add FAQ Reference help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-faq',
 		'title'   => __( 'FAQ', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-faq.php' ),
+		'content' => file_get_contents( get_template_directory() . '/help/faq.htm' ),
 	) );
 	// Add Code Ref Reference help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-coderef',
 		'title'   => __( 'Code Reference', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-coderef.php' ), 
+		'content' => file_get_contents( get_template_directory() . '/help/coderef.htm' ), 
 	) );
 	// Add Change Log Reference help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-changelog',
 		'title'   => __( 'Change Log', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-changelog.php' ),
+		'content' => file_get_contents( get_template_directory() . '/help/changelog.htm' ),
 	) );
 	// Add License Reference help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-license',
 		'title'   => __( 'License', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-license.php' ),
+		'content' => file_get_contents( get_template_directory() . '/help/license.htm' ),
 	) );
 	// Add Support Reference help screen tab
 	$screen->add_help_tab( array(
 		'id'      => 'oenology-support',
 		'title'   => __( 'Theme Support', 'oenology' ),
-		'content' => include( get_template_directory() . '/functions/contextual-help-support.php' ),
+		'content' => include( get_template_directory() . '/help/support.php' ),
 	) );
 }
 ?>
