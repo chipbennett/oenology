4191133123d47ccf961c9dd9485aad3c0a5c3895
Chip Bennett <chip@chipbennett.net>
2018-12-29 09:28:03 -0500

    Re: Issue #153 ThemeSniffer: WP-Docs cleanup
    Re: Issue #153 ThemeSniffer: WP-Docs cleanup

--- js/oenology.fitvids.js
+++ js/oenology.fitvids.js
@@ -1,3 +1,13 @@
+/**
+ * Oenology fitvids JS implementation
+ *
+ * This file includes fitvids.js integration for the Theme.
+ * 
+ * @package 	Oenology
+ * @copyright	Copyright (c) 2010, Chip Bennett
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
+ */
+
  jQuery(document).ready(function($){
     // Target your .container, .wrapper, .post, etc.
     $(".post-entry").fitVids();
--- js/oenology.tinynav.js
+++ js/oenology.tinynav.js
@@ -1,5 +1,15 @@
+/**
+ * Oenology tinynav JS implementation
+ *
+ * This file includes tinynav.js integration for the Theme.
+ * 
+ * @package 	Oenology
+ * @copyright	Copyright (c) 2010, Chip Bennett
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
+ */
+
 jQuery(".nav-header").tinyNav({
-	active: 'current_page_item', // Set the "active" class for default menu
-	label: '', // String: Sets the <label> text for the <select> (if not set, no label will be added)
-header: '' // String: Specify text for "header" and show header instead of the active item
+	active: 'current_page_item', // Set the "active" class for default menu.
+	label: '', // String: Sets the <label> text for the <select> (if not set, no label will be added).
+	header: '' // String: Specify text for "header" and show header instead of the active item.
 });
